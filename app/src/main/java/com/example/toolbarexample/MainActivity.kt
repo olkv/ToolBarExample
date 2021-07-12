522d675a3d2f869ee644c9bf350267aa04277ab2
Oleg Kvashnin <olkv@wellit.pro>
2021-07-12 23:30:14 +0600

    Одна кнопка в ToolBarMenu

--- app/src/main/java/com/example/toolbarexample/MainActivity.kt
+++ app/src/main/java/com/example/toolbarexample/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.toolbarexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init()
+
     }
+
+    private fun init() {
+        // supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.id_settings -> {
+                Toast.makeText(this,"Нажата кнопка настройки",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return true
+    }
+
 }
